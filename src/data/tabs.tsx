5ccf5eae1638faf10f30fbe0f79e9e730d6b05ca
AntonEmery <antonemery@gmail.com>
2024-07-08 21:13:19 -0700

    Add Tabs page

---
+++ src/data/tabs.tsx
@@ -0,0 +1,115 @@
+interface Tab {
+  title: string;
+  description: string;
+  tuning: string;
+  audio?: string | boolean;
+  video?: string | boolean;
+  notation: string;
+}
+
+export const Tabs: Tab[] = [
+  {
+    title: "O'Carolan's Welcome",
+    description: "A wonderful haunting harp tune by Turlough O'Carolan",
+    tuning: 'CGDGCD - Capo II',
+    audio: '/audio/ocarolans_welcome.mp3',
+    video: 'https://youtu.be/apEyOGkT2-U',
+    notation: '/tabs/ocarolans_welcome.pdf',
+  },
+  {
+    title: "Noone Lasses",
+    description: "A fun reel in G i learned from the playing of Chicago flute player John Kreaven. This tune ended up being the title track of my first album.",
+    tuning: 'CGDGCD',
+    audio: '/audio/noone_lasses.mp3',
+    video: 'https://youtu.be/hhp4iVHqY0c',
+    notation: '/tabs/noone_lasses.pdf',
+  },
+  {
+    title: "The Isolation Waltz",
+    description: "A waltz I composed that became the title track of my second album.",
+    tuning: 'CGDGCD',
+    audio: '/audio/isolation_waltz.mp3',
+    video: 'https://www.youtube.com/watch?v=RYkHtfCGw18',
+    notation: '/tabs/isolation_waltz.pdf',
+  },
+  {
+    title: "Granny In the Corner",
+    description: "A fun tune I heard Steve Baughman play one night as we were hanging out with a bottle of scotch. I made a mental note to arrange myself one day. He said he got it from Martin Hayes.",
+    tuning: 'CGDGCD',
+    audio: '/audio/granny_in_the_corner.mp3',
+    video: false,
+    notation: '/tabs/granny_in_the_corner.pdf',
+  },
+  {
+    title: "Tobin's Jig",
+    description: "This is a standard jig that you can hear at almost every irish session.",
+    tuning: 'CGDGCD - Capo II',
+    audio: '/audio/tobins_jig.mp3',
+    video: false,
+    notation: '/tabs/tobins_jig.pdf',
+  },
+  {
+    title: "Father Kelly's Reel",
+    description: 'Another popular reel that you can hear at almost every irish session.',
+    tuning: 'CGDGCD',
+    audio: '/audio/father_kellys.mp3',
+    video: false,
+    notation: '/tabs/father_kellys_reel.pdf',
+  },
+  {
+    title: "The Drunken Gauger",
+    description: "I first heard this tune on the Grey Larsen and Paddy League cd, Dark of the Moon. Apparently a gauger was someone who would go around with a ruler and make sure everyone got a fair pour when they ordered a pint. I could see how that line of work could lead to drunkedness.",
+    tuning: 'CGDGCD',
+    audio: '/audio/drunken_gauger.mp3',
+    video: 'https://youtu.be/kBOTK5Kr-m0?si=fVyv7RKVgCvQHkl0',
+    notation: '/tabs/drunken_gauger.pdf',
+  },
+  {
+    title: "Conset Y Peipar Coch/Erddigan Y Pibydd",
+    description: "These were some of the earliest celtic tunes I arranged. I came across them on a compilation album of Welsh music I found in the music library at Florida State University.",
+    tuning: 'CGDGCD',
+    audio: '/audio/Conset_Y_Peipar_Coch_Erddigan_Y_Pibydd.mp3',
+    video: 'https://youtu.be/GbVrGaN8ncg?si=yzYgGczGWVFBonnh',
+    notation: '/tabs/Conset_Y_Peipar_Coch_Erddigan_Y_Pibydd.pdf',
+  },
+  {
+    title: "O'Carolan's Receipt",
+    description: "One of the first tunes I arranged by the blind harper Turlough O'Carolan. I heard it on an album by the harpist Derek Bell.",
+    tuning: 'CGDGCD',
+    audio: '/audio/ocarolans_receipt.mp3',
+    video: 'https://youtu.be/9eE6Y_VSQPs?si=w_uBw75H_VyXq5eT',
+    notation: '/tabs/ocarolans_receipt.pdf',
+  },
+  {
+    title: "The Kesh Jig",
+    description: "Another popular session jig",
+    tuning: 'CGDGCD',
+    audio: '/audio/kesh_jig.mp3',
+    video: false,
+    notation: '/tabs/kesh_jig.pdf',
+  },
+  {
+    title: "Princess Royal",
+    description: "Another beautiful tune by Turlough O'Carolan",
+    tuning: 'CGDGCD',
+    audio: '/audio/princess_royal.mp3',
+    video: false,
+    notation: '/tabs/princess_royal.pdf',
+  },
+  {
+    title: "Bobby Casey's",
+    description: "A slow reel i heard on Kevin Burke's album, If the Cap Fits.",
+    tuning: 'CGDGCD',
+    audio: '/audio/Bobby_Caseys_Granny_In_The_Corner.mp3',
+    video: 'https://www.youtube.com/watch?v=NUkvvvl4KZo',
+    notation: '/tabs/bobby_caseys.pdf',
+  },
+  {
+    title: "The Windmill",
+    description: 'This is a reel played by Altan and composed by one of their band members i believe. I first heard it played on fingerpicked guitar by Tallahassee, FL musician Fred Slade.',
+    tuning: 'CGDGCD - Capo II',
+    audio: '/audio/windmill.mp3',
+    video: 'https://www.youtube.com/watch?v=z3_bIR7ujGY',
+    notation: '/tabs/windmill.pdf',
+  },
+];
