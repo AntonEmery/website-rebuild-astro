b81ff47b603a03d114486dc5fc3919dcfcf891e1
AntonEmery <antonemery@gmail.com>
2025-02-23 12:47:37 -0800

    Add schema to blog Collection

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,9 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
-// Define a collection of markdown file blog posts
+// Define a collection of markdown or mdx file blog posts
 const blog = defineCollection({
-  loader: glob({ pattern: "*.md", base: './src/content/blog' }),
+  loader: glob({ pattern: "*.mdx", base: './src/content/blog' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    keywords: z.string(),
+    postTitle: z.string(),
+    slug: z.string(),
+    featuredImage: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
 })
 
 export const collections = { blog };
